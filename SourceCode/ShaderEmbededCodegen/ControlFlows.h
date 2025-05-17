b2f9683be50ea8b97d5eba14179d820ede190757
GraphZou <906520989@qq.com>
2025-05-17 16:59:48 +0800

    清理代码 重订接口

--- SourceCode/ShaderEmbededCodegen/ControlFlows.h
+++ SourceCode/ShaderEmbededCodegen/ControlFlows.h
@@ -1,72 +1,40 @@
-//#pragma once
-//
-//#include"ast.h"
-//
-//
-//namespace MathLib
-//{
-//	class GPU_IF
-//	{
-//	private:
-//		CSTNode* innerCondition;
-//
-//	public:
-//
-//		GPU_IF(CSTNode* condition)
-//			: innerCondition(condition)
-//		{
-//		}
-//
-//		void operator % (std::function<void()> func)
-//		{
-//			//MathLib::runtimeAST.addNode(OperatorNodeType::IF, innerCondition);
-//			//func();
-//			//MathLib::runtimeAST.addNode(OperatorNodeType::CLOSE_SCOPE, nullptr);
-//		}
-//
-//	};
-//#define $IF(...) MathLib::GPU_IF(__VA_ARGS__) % [&]() noexcept -> void
-//
-//
-//	class GPU_ELSEIF
-//	{
-//	private:
-//		CSTNode* innerCondition;
-//
-//	public:
-//
-//		GPU_ELSEIF(CSTNode* condition) :innerCondition(condition)
-//		{
-//		}
-//
-//		void operator % (std::function<void()> func)
-//		{
-//			//MathLib::runtimeAST.addNode(OperatorNodeType::IF, innerCondition);
-//			//func();
-//			//MathLib::runtimeAST.addNode(OperatorNodeType::CLOSE_SCOPE, nullptr);
-//		}
-//	};
-//#define $ELIF(...) ;MathLib::GPU_ELSEIF(__VA_ARGS__) % [&]() noexcept -> void
-//
-//
-//	class GPU_ELSE
-//	{
-//	private:
-//		//CSTNode* innerCondition;
-//
-//	public:
-//		GPU_ELSE()
-//		{
-//
-//		}
-//
-//		void operator % (std::function<void()> func)
-//		{
-//			//MathLib::runtimeAST.addNode(OperatorNodeType::IF, innerCondition);
-//			//func();
-//			//MathLib::runtimeAST.addNode(OperatorNodeType::CLOSE_SCOPE, nullptr);
-//		}
-//	};
-//#define $ELSE ;MathLib::GPU_ELSE() % [&]() noexcept -> void
-//
-//};
+#pragma once
+
+#include "VariateProxy.h"
+
+template<typename Type>
+struct GPU_IF
+{
+	GPU_IF(VariateProxy<Type>& condition)
+	{
+	}
+	void operator % (std::function<void()> func)
+	{
+	}
+};
+#define $IF(...) ;GPU_IF(__VA_ARGS__) % [&]() noexcept -> void
+
+
+template<typename Type>
+struct GPU_ELSEIF
+{
+	GPU_ELSEIF(VariateProxy<Type>& condition)
+	{
+	}
+	void operator % (std::function<void()> func)
+	{
+	}
+};
+#define $ELIF(...) ;GPU_ELSEIF(__VA_ARGS__) % [&]() noexcept -> void
+
+
+struct GPU_ELSE
+{
+	GPU_ELSE()
+	{
+	}
+	void operator % (std::function<void()> func)
+	{
+	}
+};
+#define $ELSE ;GPU_ELSE() % [&]() noexcept -> void
--- SourceCode/main.cpp
+++ SourceCode/main.cpp
@@ -1,6 +1,58 @@
-#include "TestAst.h"
+#include <iostream>
+
+#include "ShaderEmbededCodegen/ControlFlows.h"
+
+#include "ShaderEmbededCodegen/CustomLibrary.h"
+
+
+struct MyStruct1
+{
+	VariateProxy<int> int_A = 85244;
+	VariateProxy<VariateProxy<int>> arr = { 1, 2 };
+};
+
+struct MyStruct2
+{
+	VariateProxy<int> int_A = 85244;
+	VariateProxy<VariateProxy<int>> arr = { 1, 2 };
+};
+
+struct MyStruct3
+{
+	VariateProxy<MyStruct2> int_A;
+};
+
+
 
 int main(int argc, char* argv[])
 {
-	TestAst();
+	auto lambda = [&]()
+		{
+			VariateProxy<VariateProxy<int>> arr = { 1, 2 ,8, 1, 72, 11, 48416 };
+
+			VariateProxy<MyStruct1> myStruct1;
+			VariateProxy<MyStruct2> myStruct2;
+			VariateProxy<MyStruct3> myStruct3;
+
+			myStruct1->int_A = 1;
+
+			VariateProxy<ktm::fvec3> fvec3_A = ktm::fvec3(1, 2, 3);
+			VariateProxy<ktm::fvec3> fvec3_B = ktm::fvec3(1, 2, 3);
+
+			fvec3_B = MathFunc::sqrt(fvec3_A);
+
+
+			$IF(fvec3_A)
+			{
+			};
+			$ELIF(fvec3_B)
+			{
+			};
+			$ELSE
+			{
+			};
+		};
+
+	std::string parseRes = abstractSyntaxTree.parse(lambda);
+	std::cout << parseRes;
 }
