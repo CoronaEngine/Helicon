f4538119cffac4f64e0a436ac173216e131d3c8c
与湫风起 <472266503@qq.com>
2025-07-03 16:38:27 +0800

    Update #整理代码、优化命名

--- SourceCode/ShaderGenerator/ShaderGenerator.cpp
+++
@@ -1,8 +0,0 @@
-#include <AST/AST.hpp>
-#include <ShaderGenerator/ShaderGenerator.hpp>
-
-std::string EmbeddedShader::ShaderGenerator::BaseShaderGenerator::getCodeIndentation()
-{
-	//ide可能会误报warning
-	return std::string(nestHierarchy, '\t');
-}
