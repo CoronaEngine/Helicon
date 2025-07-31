bfb62bd2e98e1caaf10a83d54385d15eaad6435a
Peter Peng <3457393079@qq.com>
2025-07-31 16:21:33 +0800

    hardcode更新

--- Src/Compiler/HardcodeShaders/HardcodeShadersHLSL.cpp
+++ Src/Compiler/HardcodeShaders/HardcodeShadersHLSL.cpp
@@ -1,3 +1,433 @@
 #include"HardcodeShaders.h"
-std::unordered_map<std::string, ShaderCodeModule> HardcodeShaders::hardcodeShadersHLSL = {
+std::unordered_map<std::string, ShaderCodeModule> HardcodeShaders::hardcodeShadersHLSL = {{"VertexShader_D__Files_Code_Projects_Helicon_Examples_main_cpp_185_32",
+ShaderCodeModule(R"(#pragma pack_matrix(column_major)
+#ifdef SLANG_HLSL_ENABLE_NVAPI
+#include "nvHLSLExtns.h"
+#endif
+
+#ifndef __DXC_VERSION_MAJOR
+// warning X3557: loop doesn't seem to do anything, forcing loop to unroll
+#pragma warning(disable : 3557)
+#endif
+
+
+#line 14 "25ca870c7e98433c9a036eadc77fd7183776e110"
+struct global_ubo_struct_0
+{
+    float4x4 global_var_0_0;
+    float3x3 global_var_1_0;
+    float4x4 global_var_2_0;
+    float4x4 global_var_3_0;
+    float3 global_var_4_0;
+    float3 global_var_5_0;
+    float3 global_var_6_0;
+    uint2 global_var_9_0;
+};
+
+
+#line 22879 "hlsl.meta.slang"
+struct GlobalParams_0
+{
+    uint2 global_var_7_0;
+    uint2 global_ubo_0;
+};
+
+
+#line 22879
+cbuffer globalParams_0 : register(b0)
+{
+    GlobalParams_0 globalParams_0;
+}
+
+#line 8 "25ca870c7e98433c9a036eadc77fd7183776e110"
+struct aggregate_type_1_0
+{
+    float3 fragPos_0;
+    float3 fragNormal_0;
+    float2 fragTexCoord_0;
+    float3 fragColor_0;
+};
+
+
+#line 8
+aggregate_type_1_0 aggregate_type_1_x24init_0(float3 fragPos_1, float3 fragNormal_1, float2 fragTexCoord_1, float3 fragColor_1)
+{
+
+#line 8
+    aggregate_type_1_0 _S1;
+    _S1.fragPos_0 = fragPos_1;
+    _S1.fragNormal_0 = fragNormal_1;
+    _S1.fragTexCoord_0 = fragTexCoord_1;
+    _S1.fragColor_0 = fragColor_1;
+
+#line 8
+    return _S1;
+}
+
+
+#line 28
+struct vertex_output_0
+{
+    float4 position_output_0 : SV_POSITION;
+    aggregate_type_1_0 var_6_0 : LOCATION0;
+};
+
+
+#line 2
+struct aggregate_type_0_0
+{
+    float3 inPosition_0;
+    float3 inNormal_0;
+    float2 inTexCoord_0;
+    float3 inColor_0;
+};
+
+
+#line 25
+struct vertex_input_0
+{
+    aggregate_type_0_0 var_0_0 : LOCATION0;
+};
+
+
+
+
+vertex_output_0 main(vertex_input_0 input_0)
+{
+
+#line 33
+    vertex_input_0 _S2 = input_0;
+
+#line 9
+    float3 _S3 = float3(0.0f, 0.0f, 0.0f);
+
+#line 35
+    aggregate_type_1_0 var_1_0 = aggregate_type_1_x24init_0(_S3, _S3, float2(0.0f, 0.0f), _S3);
+
+#line 34
+    vertex_output_0 output_0;
+
+    float4x4 _S4 = ConstantBuffer<global_ubo_struct_0 >(ResourceDescriptorHeap[globalParams_0.global_ubo_0.x]).global_var_3_0;
+
+#line 36
+    float4x4 _S5 = mul(_S4, ConstantBuffer<global_ubo_struct_0 >(ResourceDescriptorHeap[globalParams_0.global_ubo_0.x]).global_var_2_0);
+
+#line 36
+    output_0.position_output_0 = mul(mul(_S5, ConstantBuffer<global_ubo_struct_0 >(ResourceDescriptorHeap[globalParams_0.global_ubo_0.x]).global_var_0_0), float4(_S2.var_0_0.inPosition_0, 1.0f));
+
+#line 41
+    output_0.var_6_0 = var_1_0;
+    return output_0;
+}
+
+ )")
+},{"FragmentShader_D__Files_Code_Projects_Helicon_Examples_main_cpp_186_34",
+ShaderCodeModule(R"(#pragma pack_matrix(column_major)
+#ifdef SLANG_HLSL_ENABLE_NVAPI
+#include "nvHLSLExtns.h"
+#endif
+
+#ifndef __DXC_VERSION_MAJOR
+// warning X3557: loop doesn't seem to do anything, forcing loop to unroll
+#pragma warning(disable : 3557)
+#endif
+
+
+#line 14 "0b013cac2bbb006311909e1f8466712f91e3d97f"
+struct global_ubo_struct_0
+{
+    float4x4 global_var_0_0;
+    float3x3 global_var_1_0;
+    float4x4 global_var_2_0;
+    float4x4 global_var_3_0;
+    float3 global_var_4_0;
+    float3 global_var_5_0;
+    float3 global_var_6_0;
+    uint2 global_var_9_0;
+};
+
+
+#line 22879 "hlsl.meta.slang"
+struct GlobalParams_0
+{
+    uint2 global_var_7_0;
+    uint2 global_ubo_0;
+};
+
+
+#line 22879
+cbuffer globalParams_0 : register(b0)
+{
+    GlobalParams_0 globalParams_0;
+}
+
+#line 28 "0b013cac2bbb006311909e1f8466712f91e3d97f"
+struct fragment_output_0
+{
+    float4 var_32_0 : SV_TARGET0;
+};
+
+
+#line 8
+struct aggregate_type_1_0
+{
+    float3 fragPos_0;
+    float3 fragNormal_0;
+    float2 fragTexCoord_0;
+    float3 fragColor_0;
+};
+
+
+#line 25
+struct fragment_input_0
+{
+    aggregate_type_1_0 var_0_0 : LOCATION0;
+};
+
+
+
+fragment_output_0 main(fragment_input_0 input_0)
+{
+
+#line 32
+    fragment_input_0 _S1 = input_0;
+    fragment_output_0 output_0;
+
+    Texture2D<float4 > _S2 = Texture2D<float4 >(ResourceDescriptorHeap[globalParams_0.global_var_7_0.x]);
+
+#line 35
+    SamplerState _S3 = SamplerState(SamplerDescriptorHeap[ConstantBuffer<global_ubo_struct_0 >(ResourceDescriptorHeap[globalParams_0.global_ubo_0.x]).global_var_9_0.x]);
+
+#line 35
+    float3 var_1_0;
+
+#line 35
+    if((_S2.Sample(_S3, _S1.var_0_0.fragTexCoord_0).w) > 0.00999999977648258f)
+    {
+
+#line 36
+        Texture2D<float4 > _S4 = Texture2D<float4 >(ResourceDescriptorHeap[globalParams_0.global_var_7_0.x]);
+
+#line 36
+        SamplerState _S5 = SamplerState(SamplerDescriptorHeap[ConstantBuffer<global_ubo_struct_0 >(ResourceDescriptorHeap[globalParams_0.global_ubo_0.x]).global_var_9_0.x]);
+
+#line 36
+        var_1_0 = _S4.Sample(_S5, _S1.var_0_0.fragTexCoord_0).xyz;
+
+#line 35
+    }
+    else
+    {
+
+#line 35
+        var_1_0 = _S1.var_0_0.fragColor_0;
+
+#line 35
+    }
+
+#line 44
+    float3 var_4_0 = lerp(float3(0.03999999910593033f, 0.03999999910593033f, 0.03999999910593033f), var_1_0, (float3)0.5f);
+
+#line 52
+    float3 _S6 = normalize(_S1.var_0_0.fragNormal_0);
+
+#line 52
+    float3 _S7 = normalize(ConstantBuffer<global_ubo_struct_0 >(ResourceDescriptorHeap[globalParams_0.global_ubo_0.x]).global_var_4_0 - _S1.var_0_0.fragPos_0);
+
+#line 52
+    float _S8 = max(dot(_S6, normalize(_S7 + normalize(ConstantBuffer<global_ubo_struct_0 >(ResourceDescriptorHeap[globalParams_0.global_ubo_0.x]).global_var_6_0 - _S1.var_0_0.fragPos_0))), 0.0f);
+
+#line 52
+    float3 _S9 = normalize(ConstantBuffer<global_ubo_struct_0 >(ResourceDescriptorHeap[globalParams_0.global_ubo_0.x]).global_var_4_0 - _S1.var_0_0.fragPos_0);
+
+#line 52
+    float _S10 = 3.14159297943115234f * (_S8 * max(dot(_S6, normalize(_S9 + normalize(ConstantBuffer<global_ubo_struct_0 >(ResourceDescriptorHeap[globalParams_0.global_ubo_0.x]).global_var_6_0 - _S1.var_0_0.fragPos_0))), 0.0f) * -0.9375f + 1.0f);
+
+#line 52
+    float3 _S11 = normalize(ConstantBuffer<global_ubo_struct_0 >(ResourceDescriptorHeap[globalParams_0.global_ubo_0.x]).global_var_4_0 - _S1.var_0_0.fragPos_0);
+
+#line 52
+    float _S12 = max(dot(_S6, normalize(_S11 + normalize(ConstantBuffer<global_ubo_struct_0 >(ResourceDescriptorHeap[globalParams_0.global_ubo_0.x]).global_var_6_0 - _S1.var_0_0.fragPos_0))), 0.0f);
+
+#line 52
+    float3 _S13 = normalize(ConstantBuffer<global_ubo_struct_0 >(ResourceDescriptorHeap[globalParams_0.global_ubo_0.x]).global_var_4_0 - _S1.var_0_0.fragPos_0);
+
+#line 52
+    float var_12_0 = _S10 * (_S12 * max(dot(_S6, normalize(_S13 + normalize(ConstantBuffer<global_ubo_struct_0 >(ResourceDescriptorHeap[globalParams_0.global_ubo_0.x]).global_var_6_0 - _S1.var_0_0.fragPos_0))), 0.0f) * -0.9375f + 1.0f);
+    float var_13_0 = max(dot(_S6, normalize(ConstantBuffer<global_ubo_struct_0 >(ResourceDescriptorHeap[globalParams_0.global_ubo_0.x]).global_var_4_0 - _S1.var_0_0.fragPos_0)), 0.0f);
+
+#line 58
+    float var_18_0 = max(dot(_S6, normalize(ConstantBuffer<global_ubo_struct_0 >(ResourceDescriptorHeap[globalParams_0.global_ubo_0.x]).global_var_6_0 - _S1.var_0_0.fragPos_0)), 0.0f);
+
+#line 64
+    float3 var_24_0 = float3(1.0f, 1.0f, 1.0f);
+
+
+    float3 _S14 = var_24_0 - var_4_0;
+
+#line 67
+    float3 _S15 = normalize(ConstantBuffer<global_ubo_struct_0 >(ResourceDescriptorHeap[globalParams_0.global_ubo_0.x]).global_var_4_0 - _S1.var_0_0.fragPos_0);
+
+#line 67
+    float3 _S16 = normalize(_S15 + normalize(ConstantBuffer<global_ubo_struct_0 >(ResourceDescriptorHeap[globalParams_0.global_ubo_0.x]).global_var_6_0 - _S1.var_0_0.fragPos_0));
+
+
+
+    float3 _S17 = (var_24_0 - (var_4_0 + _S14 * pow(clamp(1.0f - clamp(dot(_S16, normalize(ConstantBuffer<global_ubo_struct_0 >(ResourceDescriptorHeap[globalParams_0.global_ubo_0.x]).global_var_4_0 - _S1.var_0_0.fragPos_0)), 0.0f, 1.0f), 0.0f, 1.0f), 5.0f))) * var_1_0 / 3.14159297943115234f;
+
+#line 71
+    float _S18 = 0.0625f / var_12_0 * (var_18_0 / (var_18_0 * 0.71875f + 0.28125f) * (var_13_0 / (var_13_0 * 0.71875f + 0.28125f)));
+
+#line 71
+    float3 _S19 = normalize(ConstantBuffer<global_ubo_struct_0 >(ResourceDescriptorHeap[globalParams_0.global_ubo_0.x]).global_var_4_0 - _S1.var_0_0.fragPos_0);
+
+#line 71
+    float3 _S20 = normalize(_S19 + normalize(ConstantBuffer<global_ubo_struct_0 >(ResourceDescriptorHeap[globalParams_0.global_ubo_0.x]).global_var_6_0 - _S1.var_0_0.fragPos_0));
+
+#line 71
+    float3 _S21 = _S18 * (var_4_0 + _S14 * pow(clamp(1.0f - clamp(dot(_S20, normalize(ConstantBuffer<global_ubo_struct_0 >(ResourceDescriptorHeap[globalParams_0.global_ubo_0.x]).global_var_4_0 - _S1.var_0_0.fragPos_0)), 0.0f, 1.0f), 0.0f, 1.0f), 5.0f));
+
+#line 71
+    float _S22 = 4.0f * max(dot(_S6, normalize(ConstantBuffer<global_ubo_struct_0 >(ResourceDescriptorHeap[globalParams_0.global_ubo_0.x]).global_var_4_0 - _S1.var_0_0.fragPos_0)), 0.0f);
+
+#line 71
+    float3 _S23 = _S17 + _S21 / (_S22 * max(dot(_S6, normalize(ConstantBuffer<global_ubo_struct_0 >(ResourceDescriptorHeap[globalParams_0.global_ubo_0.x]).global_var_6_0 - _S1.var_0_0.fragPos_0)), 0.0f) + 0.00009999999747379f);
+
+#line 71
+    float3 _S24 = _S23 * ConstantBuffer<global_ubo_struct_0 >(ResourceDescriptorHeap[globalParams_0.global_ubo_0.x]).global_var_5_0;
+
+    output_0.var_32_0 = float4(float3(0.02999999932944775f, 0.02999999932944775f, 0.02999999932944775f) * var_1_0 + _S24 * max(dot(_S6, normalize(ConstantBuffer<global_ubo_struct_0 >(ResourceDescriptorHeap[globalParams_0.global_ubo_0.x]).global_var_6_0 - _S1.var_0_0.fragPos_0)), 0.0f), 1.0f);
+    return output_0;
+}
+
+ )")
+},{"ComputeShader_D__Files_Code_Projects_Helicon_Examples_main_cpp_187_34",
+ShaderCodeModule(R"(#pragma pack_matrix(column_major)
+#ifdef SLANG_HLSL_ENABLE_NVAPI
+#include "nvHLSLExtns.h"
+#endif
+
+#ifndef __DXC_VERSION_MAJOR
+// warning X3557: loop doesn't seem to do anything, forcing loop to unroll
+#pragma warning(disable : 3557)
+#endif
+
+
+#line 22879 "hlsl.meta.slang"
+struct GlobalParams_0
+{
+    uint2 global_var_7_0;
+};
+
+
+#line 22879
+cbuffer globalParams_0 : register(b0)
+{
+    GlobalParams_0 globalParams_0;
+}
+
+#line 2 "b766333db470cb507842e8f34c6b71704f6ebe5a"
+struct compute_input_0
+{
+    uint3 dispatch_thread_id_input_0 : SV_DispatchThreadID;
+};
+
+[numthreads(8, 8, 1)]
+void main(compute_input_0 input_0)
+{
+
+#line 7
+    compute_input_0 _S1 = input_0;
+
+#line 13
+    RWTexture2D<float4 > _S2 = RWTexture2D<float4 >(ResourceDescriptorHeap[globalParams_0.global_var_7_0.x]);
+
+#line 13
+    uint2 _S3 = _S1.dispatch_thread_id_input_0.xy;
+
+#line 13
+    RWTexture2D<float4 > _S4 = RWTexture2D<float4 >(ResourceDescriptorHeap[globalParams_0.global_var_7_0.x]);
+
+#line 13
+    float4 _S5 = _S4[_S3];
+
+#line 13
+    float3 _S6 = _S5.xyz;
+
+#line 13
+    RWTexture2D<float4 > _S7 = RWTexture2D<float4 >(ResourceDescriptorHeap[globalParams_0.global_var_7_0.x]);
+
+#line 13
+    float4 _S8 = _S7[_S3];
+
+#line 13
+    float3 _S9 = _S6 * (0.02999999932944775f + 2.50999999046325684f * _S8.xyz);
+
+#line 13
+    RWTexture2D<float4 > _S10 = RWTexture2D<float4 >(ResourceDescriptorHeap[globalParams_0.global_var_7_0.x]);
+
+#line 13
+    float4 _S11 = _S10[_S3];
+
+#line 13
+    float3 _S12 = _S11.xyz;
+
+#line 13
+    RWTexture2D<float4 > _S13 = RWTexture2D<float4 >(ResourceDescriptorHeap[globalParams_0.global_var_7_0.x]);
+
+#line 13
+    float4 _S14 = _S13[_S3];
+
+#line 13
+    float3 _S15 = float3(0.0f, 0.0f, 0.0f);
+
+#line 13
+    float3 _S16 = float3(1.0f, 1.0f, 1.0f);
+
+#line 13
+    _S2[_S3] = float4(clamp(_S9 / (0.14000000059604645f + _S12 * (0.5899999737739563f + 2.43000006675720215f * _S14.xyz)), _S15, _S16), 1.0f);
+
+#line 19
+    RWTexture2D<float4 > _S17 = RWTexture2D<float4 >(ResourceDescriptorHeap[globalParams_0.global_var_7_0.x]);
+
+#line 19
+    RWTexture2D<float4 > _S18 = RWTexture2D<float4 >(ResourceDescriptorHeap[globalParams_0.global_var_7_0.x]);
+
+#line 19
+    float4 _S19 = _S18[_S3];
+
+#line 19
+    float3 _S20 = _S19.xyz;
+
+#line 19
+    RWTexture2D<float4 > _S21 = RWTexture2D<float4 >(ResourceDescriptorHeap[globalParams_0.global_var_7_0.x]);
+
+#line 19
+    float4 _S22 = _S21[_S3];
+
+#line 19
+    float3 _S23 = _S20 * (0.02999999932944775f + 2.50999999046325684f * _S22.xyz);
+
+#line 19
+    RWTexture2D<float4 > _S24 = RWTexture2D<float4 >(ResourceDescriptorHeap[globalParams_0.global_var_7_0.x]);
+
+#line 19
+    float4 _S25 = _S24[_S3];
+
+#line 19
+    float3 _S26 = _S25.xyz;
+
+#line 19
+    RWTexture2D<float4 > _S27 = RWTexture2D<float4 >(ResourceDescriptorHeap[globalParams_0.global_var_7_0.x]);
+
+#line 19
+    float4 _S28 = _S27[_S3];
+
+#line 19
+    _S17[_S3] = float4(clamp(_S23 / (0.14000000059604645f + _S26 * (0.5899999737739563f + 2.43000006675720215f * _S28.xyz)), _S15, _S16), 1.0f);
+    return;
+}
+
+ )")
+},
 };
--- Src/Compiler/HardcodeShaders/HardcodeShadersSlang.cpp
+++ Src/Compiler/HardcodeShaders/HardcodeShadersSlang.cpp
@@ -1,3 +1,147 @@
 #include"HardcodeShaders.h"
-std::unordered_map<std::string, ShaderCodeModule> HardcodeShaders::hardcodeShadersSlang = {
+std::unordered_map<std::string, ShaderCodeModule> HardcodeShaders::hardcodeShadersSlang = {{"VertexShader_D__Files_Code_Projects_Helicon_Examples_main_cpp_185_32",
+ShaderCodeModule(R"(uniform Texture2D<float4>.Handle global_var_7;
+struct aggregate_type_0 {
+	float3 inPosition;
+	float3 inNormal;
+	float2 inTexCoord;
+	float3 inColor;
+}
+struct aggregate_type_1 {
+	float3 fragPos;
+	float3 fragNormal;
+	float2 fragTexCoord;
+	float3 fragColor;
+}
+struct global_ubo_struct {
+	float4x4 global_var_0;
+	float3x3 global_var_1;
+	float4x4 global_var_2;
+	float4x4 global_var_3;
+	float3 global_var_4;
+	float3 global_var_5;
+	float3 global_var_6;
+	SamplerState.Handle global_var_9;
+}
+uniform ConstantBuffer<global_ubo_struct>.Handle global_ubo;
+struct vertex_input {
+	aggregate_type_0 var_0 : LOCATION0;
+}
+struct vertex_output {
+	float4 position_output : SV_POSITION;
+	aggregate_type_1 var_6 : LOCATION0;
+}
+[shader("vertex")]
+vertex_output main(vertex_input input) {
+	vertex_output output;
+	aggregate_type_1 var_1 = {};
+	output.position_output = mul(mul(mul((*global_ubo).global_var_3,(*global_ubo).global_var_2),(*global_ubo).global_var_0),float4(input.var_0.inPosition,1.000000));
+	float3 var_2 = mul((*global_ubo).global_var_0,float4(input.var_0.inPosition,1.000000)).xyz;
+	float3 var_3 = normalize(mul((*global_ubo).global_var_1,input.var_0.inNormal));
+	float3 var_4 = input.var_0.inColor;
+	float2 var_5 = input.var_0.inTexCoord;
+	output.var_6 = var_1;
+	return output;
+}
+ )")
+},{"FragmentShader_D__Files_Code_Projects_Helicon_Examples_main_cpp_186_34",
+ShaderCodeModule(R"(uniform Texture2D<float4>.Handle global_var_7;
+struct aggregate_type_0 {
+	float3 inPosition;
+	float3 inNormal;
+	float2 inTexCoord;
+	float3 inColor;
+}
+struct aggregate_type_1 {
+	float3 fragPos;
+	float3 fragNormal;
+	float2 fragTexCoord;
+	float3 fragColor;
+}
+struct global_ubo_struct {
+	float4x4 global_var_0;
+	float3x3 global_var_1;
+	float4x4 global_var_2;
+	float4x4 global_var_3;
+	float3 global_var_4;
+	float3 global_var_5;
+	float3 global_var_6;
+	SamplerState.Handle global_var_9;
+}
+uniform ConstantBuffer<global_ubo_struct>.Handle global_ubo;
+struct fragment_input {
+	aggregate_type_1 var_0 : LOCATION0;
+}
+struct fragment_output {
+	float4 var_32 : SV_TARGET0;
+}
+[shader("fragment")]
+fragment_output main(fragment_input input) {
+	fragment_output output;
+	float3 var_1 = float3(0.000000, 0.000000, 0.000000);
+	if ((global_var_7.Sample((*global_ubo).global_var_9,input.var_0.fragTexCoord).w > 0.010000)) {
+		var_1 = global_var_7.Sample((*global_ubo).global_var_9,input.var_0.fragTexCoord).xyz;
+	}
+	else {
+		var_1 = input.var_0.fragColor;
+	}
+	float var_2 = 0.500000;
+	float var_3 = 0.500000;
+	float3 var_4 = float3(0.040000, 0.040000, 0.040000);
+	var_4 = lerp(var_4,var_1,var_3);
+	float3 var_5 = float3(0.000000, 0.000000, 0.000000);
+	float var_6 = 1.000000;
+	float var_7 = var_2;
+	float var_8 = ((var_7 * var_7) * (var_7 * var_7));
+	float var_9 = 1.000000;
+	float var_10 = 1.000000;
+	float var_11 = 3.141593;
+	float var_12 = ((var_11 * (((max(dot(normalize(input.var_0.fragNormal),normalize((normalize(((*global_ubo).global_var_4 - input.var_0.fragPos)) + normalize(((*global_ubo).global_var_6 - input.var_0.fragPos))))),0.000000) * max(dot(normalize(input.var_0.fragNormal),normalize((normalize(((*global_ubo).global_var_4 - input.var_0.fragPos)) + normalize(((*global_ubo).global_var_6 - input.var_0.fragPos))))),0.000000)) * (((var_7 * var_7) * (var_7 * var_7)) - var_9)) + var_10)) * (((max(dot(normalize(input.var_0.fragNormal),normalize((normalize(((*global_ubo).global_var_4 - input.var_0.fragPos)) + normalize(((*global_ubo).global_var_6 - input.var_0.fragPos))))),0.000000) * max(dot(normalize(input.var_0.fragNormal),normalize((normalize(((*global_ubo).global_var_4 - input.var_0.fragPos)) + normalize(((*global_ubo).global_var_6 - input.var_0.fragPos))))),0.000000)) * (((var_7 * var_7) * (var_7 * var_7)) - var_9)) + var_10));
+	float var_13 = max(dot(normalize(input.var_0.fragNormal),normalize(((*global_ubo).global_var_4 - input.var_0.fragPos))),0.000000);
+	float var_14 = 1.000000;
+	float var_15 = 8.000000;
+	float var_16 = var_13;
+	float var_17 = 1.000000;
+	float var_18 = max(dot(normalize(input.var_0.fragNormal),normalize(((*global_ubo).global_var_6 - input.var_0.fragPos))),0.000000);
+	float var_19 = 1.000000;
+	float var_20 = 8.000000;
+	float var_21 = var_18;
+	float var_22 = 1.000000;
+	float var_23 = 1.000000;
+	float3 var_24 = float3(1.000000, 1.000000, 1.000000);
+	float var_25 = 4.000000;
+	float var_26 = 0.000100;
+	float3 var_27 = (var_4 + ((var_24 - var_4) * pow(clamp((var_23 - clamp(dot(normalize((normalize(((*global_ubo).global_var_4 - input.var_0.fragPos)) + normalize(((*global_ubo).global_var_6 - input.var_0.fragPos)))),normalize(((*global_ubo).global_var_4 - input.var_0.fragPos))),0.000000,1.000000)),0.000000,1.000000),5.000000)));
+	float3 var_28 = float3(1.000000, 1.000000, 1.000000);
+	float var_29 = 1.000000;
+	float var_30 = 3.141593;
+	var_5 = (var_5 + ((((((var_28 - var_27) * var_1) / var_30) + ((((var_8 / var_12) * ((var_21 / ((var_18 * (var_22 - (((var_2 + var_19) * (var_2 + var_19)) / var_20))) + (((var_2 + var_19) * (var_2 + var_19)) / var_20))) * (var_16 / ((var_13 * (var_17 - (((var_2 + var_14) * (var_2 + var_14)) / var_15))) + (((var_2 + var_14) * (var_2 + var_14)) / var_15))))) * (var_4 + ((var_24 - var_4) * pow(clamp((var_23 - clamp(dot(normalize((normalize(((*global_ubo).global_var_4 - input.var_0.fragPos)) + normalize(((*global_ubo).global_var_6 - input.var_0.fragPos)))),normalize(((*global_ubo).global_var_4 - input.var_0.fragPos))),0.000000,1.000000)),0.000000,1.000000),5.000000)))) / (((var_25 * max(dot(normalize(input.var_0.fragNormal),normalize(((*global_ubo).global_var_4 - input.var_0.fragPos))),0.000000)) * max(dot(normalize(input.var_0.fragNormal),normalize(((*global_ubo).global_var_6 - input.var_0.fragPos))),0.000000)) + var_26))) * ((*global_ubo).global_var_5 * var_6)) * max(dot(normalize(input.var_0.fragNormal),normalize(((*global_ubo).global_var_6 - input.var_0.fragPos))),0.000000)));
+	float3 var_31 = float3(0.030000, 0.030000, 0.030000);
+	output.var_32 = float4(((var_31 * var_1) + var_5),1.000000);
+	return output;
+}
+ )")
+},{"ComputeShader_D__Files_Code_Projects_Helicon_Examples_main_cpp_187_34",
+ShaderCodeModule(R"(uniform RWTexture2D<float4>.Handle global_var_7;
+struct compute_input {
+	uint3 dispatch_thread_id_input : SV_DispatchThreadID;
+}
+[shader("compute")]
+[numthreads(8,8,1)]
+void main(compute_input input) {
+	float var_0 = 2.510000;
+	float var_1 = 0.030000;
+	float var_2 = 2.430000;
+	float var_3 = 0.590000;
+	float var_4 = 0.140000;
+	global_var_7[input.dispatch_thread_id_input.xy] = float4(clamp(((global_var_7[input.dispatch_thread_id_input.xy].xyz * (var_1 + (var_0 * global_var_7[input.dispatch_thread_id_input.xy].xyz))) / (var_4 + (global_var_7[input.dispatch_thread_id_input.xy].xyz * (var_3 + (var_2 * global_var_7[input.dispatch_thread_id_input.xy].xyz))))),float3(0.000000, 0.000000, 0.000000),float3(1.000000, 1.000000, 1.000000)),1.000000);
+	float var_5 = 2.510000;
+	float var_6 = 0.030000;
+	float var_7 = 2.430000;
+	float var_8 = 0.590000;
+	float var_9 = 0.140000;
+	global_var_7[input.dispatch_thread_id_input.xy] = float4(clamp(((global_var_7[input.dispatch_thread_id_input.xy].xyz * (var_6 + (var_5 * global_var_7[input.dispatch_thread_id_input.xy].xyz))) / (var_9 + (global_var_7[input.dispatch_thread_id_input.xy].xyz * (var_8 + (var_7 * global_var_7[input.dispatch_thread_id_input.xy].xyz))))),float3(0.000000, 0.000000, 0.000000),float3(1.000000, 1.000000, 1.000000)),1.000000);
+}
+ )")
+},
 };
