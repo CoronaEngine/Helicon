2ab29020c0b5c6645e07597ea96be22018ddb8f0
Peter Peng <3457393079@qq.com>
2025-08-02 10:37:53 +0800

    添加生成好的Hardcode

---
+++ Src/Compiler/HardcodeShaders/HardcodeShaders.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <unordered_map>
+#include "../ShaderCodeCompiler.h"
+class HardcodeShaders
+{
+	friend class ShaderHardcodeManager;
+	static std::unordered_map<std::string,std::unordered_map<std::string, std::variant<ShaderCodeModule::ShaderResources,std::variant<std::vector<uint32_t>,std::string>>>*> hardcodeShaders;	static std::unordered_map<std::string, std::variant<ShaderCodeModule::ShaderResources,std::variant<std::vector<uint32_t>,std::string>>> hardcodeShadersVertexShader;	static std::unordered_map<std::string, std::variant<ShaderCodeModule::ShaderResources,std::variant<std::vector<uint32_t>,std::string>>> hardcodeShadersFragmentShader;	static std::unordered_map<std::string, std::variant<ShaderCodeModule::ShaderResources,std::variant<std::vector<uint32_t>,std::string>>> hardcodeShadersComputeShader;
+};
