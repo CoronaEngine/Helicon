8371e5017717c0999cd946d62f2bc5ad0d81b597
FriesPeter <3457393079@qq.com>
2025-08-13 23:47:01 +0800

    补充Hardcode

---
+++ Src/Compiler/HardcodeShaders/HardcodeShaders.cpp
@@ -0,0 +1,3 @@
+#include"HardcodeShaders.h"
+std::unordered_map<std::string,std::unordered_map<std::string, std::variant<EmbeddedShader::ShaderCodeModule::ShaderResources,std::variant<std::vector<uint32_t>,std::string>>>*> EmbeddedShader::HardcodeShaders::hardcodeShaders = {{"VertexShader",&hardcodeShadersVertexShader},{"FragmentShader",&hardcodeShadersFragmentShader},{"ComputeShader",&hardcodeShadersComputeShader},
+};
